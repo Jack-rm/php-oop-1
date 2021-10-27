a751b984219da9e71e9b24b1af917c7c39aa8784
Jack-rm <jackcnsr94@gmail.com>
2021-10-27 16:16:27 +0200

    Printing Movie class properties

--- index.php
+++ index.php
@@ -72,8 +72,60 @@
         }
     };
 
-    $TheMatrix = new Movie(25, "The Matrix", "Thomas A. Anderson is a man living two lives. By day he is an average computer programmer and by night a hacker known as Neo. Neo has always questioned his reality, but the truth is far beyond his imagination. Neo finds himself targeted by the police when he is contacted by Morpheus, a legendary computer hacker branded a terrorist by the government. As a rebel against the machines, Neo must confront the agents: super-powerful computer programs devoted to stopping Neo and the entire human rebellion", "Sci-fi", 136, "04/23/1999", "The Wachowskis", ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss", "Hugo Weaving", "Joe Pantoliano"]);
+    $theMatrix = new Movie(25, "The Matrix", "Thomas A. Anderson is a man living two lives. By day he is an average computer programmer and by night a hacker known as Neo. Neo has always questioned his reality, but the truth is far beyond his imagination. Neo finds himself targeted by the police when he is contacted by Morpheus, a legendary computer hacker branded a terrorist by the government. As a rebel against the machines, Neo must confront the agents: super-powerful computer programs devoted to stopping Neo and the entire human rebellion", "Sci-fi", 136, "04/23/1999", "The Wachowskis", ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss", "Hugo Weaving", "Joe Pantoliano"]);
     
     $Dune = new Movie(30, "Dune","A mythic and emotionally charged hero\'s journey, Dune tells the story of Paul Atreides, a brilliant and gifted young man born into a great destiny beyond his understanding, who must travel to the most dangerous planet in the universe to ensure the future of his family and his people. As malevolent forces explode into conflict over the planet's exclusive supply of the most precious resource in existence-a commodity capable of unlocking humanity's greatest potential-only those who can conquer their fear will survive", "science fiction", 156, "09/03/2021", "Denis Villenueve", ["Timothèe Chalamet", "Rebecca Ferguson", "Oscar Isaac", "Josh Brolin", "Stellan Skarsgàrd", "Dave Bautista", "Stephen McKinley"]);
 ;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+</head>
+<body>
+    <header>
+
+    </header>
+
+    <main>
+        <section>
+            <div>
+                <article>
+                    <h2> <?= $theMatrix->getTitle(); ?> </h2>
+                    <h5>( <?= $theMatrix->getGenre(); ?> )</h5>
+                    <h4> <?= $theMatrix->getDirector(); ?> </h4>
+                    
+                    <p> attori </p>
+                    <time>Screen Time: <?= $theMatrix->getRunTime() ; ?> </time>
+                    <p>Release Date: <?= $theMatrix->release_date ; ?> </p>
+                    <p> <?php echo $theMatrix->plot ; ?> </p>
+                </article>
+            </div>
+            <hr>
+            <div>
+                <article>
+                    <h2> <?= $Dune->getTitle() ; ?> </h2>
+                    <h5>( <?php echo $Dune->getGenre(); ?> )</h5>
+                    <h4> <?= $Dune->getDirector() ; ?> </h4>
+                   
+                    <?php foreach($Dune->actors as $actor); { ?>
+                    
+                    <p> <?php echo $actor; ?> </p>
+                    
+                    <?php }; ?>
+                    
+                    <time> Screen Time: <?= $Dune->getRunTime() ; ?> </time>
+                    <p>Release Date: <?php echo $Dune->release_date; ?> </p>
+                    <p> <?php echo $Dune->plot; ?>
+                     </p>
+                </article>
+            </div>
+        </section>
+    </main>
+    
+</body>
+</html>
